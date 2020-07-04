054d7e69b69767c556e899ae64eaa03a6c1be52c
samuelikz <samuel.silva@a.ficr.edu.br>
2020-07-04 12:45:02 -0300

    Araquivos locais

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+/*
+var d= new Date();
+alert(d.getMinutes());
+alert(d.getMonth());
+alert(d.getDay());
+*/
+
+/*
+var count;
+for(count=1; count <=5; count++){
+    alert(count);
+}
+*/
+
+//var count = 0;
+//while (count <= 5 ){
+    //console.log(count);
+  //  count++;
+//}
+
+/* var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("Maior de Idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+//var fruta = [{nome:"maça", cor:"Vermelha"}, {nome:"Uva", cor:"roxa"}]
+ //console.log(fruta)
+ //console.log(fruta.cor)
+ //console.log(fruta[1].nome)
+
+
+//var lista = ["maça","pêra","Laranja"]
+//console.log(lista)
+//lista.push("Uva")
+//lista.pop()
+//alert(lista[1])
+//console.log(lista.reverse())
+//console.log(lista.length)
+//console.log(lista.toString())
+//console.log(lista.join("-"))
+
+
+
+/*
+var nome = 'Samuel'
+var idade = 23
+var idade2 = 10
+var frase = "Café é a melhor coisa do mundo"
+
+//alert(nome +' tem '+idade+' anos')
+//alert(idade+idade2)
+console.log(nome)
+console.log(idade+idade2)
+console.log(frase.toUpperCase())
+//console.log(frase.replace("Café", "Açucar"))
+*/
